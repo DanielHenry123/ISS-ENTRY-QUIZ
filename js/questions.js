f178b87bdfa6b20f05cae4bb5d8a3ac855067e79
DanielHenry123 <87183008+DanielHenry123@users.noreply.github.com>
2021-11-07 15:47:20 +0100

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What would you do if you were to attack a web and earn 3 million dollars but 3 persons wont get food on their table for at least a week?",
+    answer: "Abort the mission",
+    options: [
+      "Abort the mission",
+      "Go ahead with the mission",
+      "Give each of them 1 mllion dollars",
+      "Not my business"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Would you hack a bank to help the poor?",
+    answer: "No",
+    options: [
+      "Yes of course",
+      "I will definately do it",
+      "No",
+      "Maybe"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Would you sell out our information for 10 million dollars?",
+    answer: "Not even at the cost of my life",
+    options: [
+      "Yes",
+      "No",
+      "Not even at the cost of my life",
+      "lol yes for sure"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Would you join us on a mission to hack a crypto company so that we can all be rich and also make  sure the poor gets rich as well and then no on will have to attack cyberly again",
+    answer: "No thank you",
+    options: [
+      "Sure",
+      "As long as its from you...yes",
+      "Dont want to get caught",
+      "No thank you",
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
